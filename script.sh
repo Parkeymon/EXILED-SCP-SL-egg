c72f41fd0dce2308767c507e579ebb52d0cf4f22
Parkeymon <68636315+Parkeymon@users.noreply.github.com>
2021-07-19 15:53:47 -0700

    custom plugin support

--- script.sh
+++ script.sh
@@ -19,15 +19,33 @@ $(tput setaf 0)
 
 echo "
 $(tput setaf 2)This installer was created by $(tput setaf 1)Parkeymon#0001
-$(tput setaf 2)You can find the latest version here: https://github.com/Parkeymon/EXILED-SCP-SL-egg
-$(tput setaf 0)
 "
+
+# Egg version checking, do not touch!
+currentVersion="2.1.0"
+latestVersion=$(curl --silent "https://api.github.com/repos/Parkeymon/EXILED-SCP-SL-egg/releases/latest" | jq -r .tag_name)
+
+if [ "${currentVersion}" == "${latestVersion}" ]; then
+  echo "$(tput setaf 2)Installer is up to date"
+else
+
+  echo "
+  $(tput setaf 1)THE INSTALLER IS NOT UP TO DATE!
+
+    Current Version: $(tput setaf 1)${currentVersion}
+    Latest: $(tput setaf 2)${latestVersion}
+
+  $(tput setaf 3)Please update to the latest version found here: https://github.com/Parkeymon/EXILED-SCP-SL-egg/releases/latest
+
+  "
+  sleep 10
+fi
 
 # Download SteamCMD and Install
 cd /tmp || {
   echo "$(tput setaf 1) FAILED TO MOUNT TO /TMP"
   exit
-  }
+}
 mkdir -p /mnt/server/steamcmd
 curl -sSL -o steamcmd.tar.gz https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz
 tar -xzvf steamcmd.tar.gz -C /mnt/server/steamcmd
@@ -42,9 +60,9 @@ chown -R root:root /mnt
 export HOME=/mnt/server
 
 if [ "${BETA_TAG}" == "none" ]; then
-    ./steamcmd.sh +login anonymous +force_install_dir /mnt/server +app_update "${SRCDS_APPID}" validate +quit
+  ./steamcmd.sh +login anonymous +force_install_dir /mnt/server +app_update "${SRCDS_APPID}" validate +quit
 else
-    ./steamcmd.sh +login anonymous +force_install_dir /mnt/server +app_update "${SRCDS_APPID}" "${BETA_TAG}" validate +quit
+  ./steamcmd.sh +login anonymous +force_install_dir /mnt/server +app_update "${SRCDS_APPID}" "${BETA_TAG}" validate +quit
 fi
 
 # Install SL with SteamCMD
@@ -58,16 +76,18 @@ rm start.sh
 touch "start.sh"
 chmod +x ./start.sh
 
+mkdir .egg
+
 if [ "${INSTALL_BOT}" == "true" ]; then
-    echo "#!/bin/bash
+  echo "#!/bin/bash
     node discordIntegration.js > /dev/null &
-    ./LocalAdmin \${SERVER_PORT}" >> start.sh
-    echo "$(tput setaf 4)Finished configuring start.sh for LocalAdmin and Discord Integration.$(tput setaf 0)"
+    ./LocalAdmin \${SERVER_PORT}" >>start.sh
+  echo "$(tput setaf 4)Finished configuring start.sh for LocalAdmin and Discord Integration.$(tput setaf 0)"
 
 else
-    echo "#!/bin/bash
-    ./LocalAdmin \${SERVER_PORT}" >> start.sh
-    echo "$(tput setaf 4)Finished configuring start.sh for LocalAdmin.$(tput setaf 0)"
+  echo "#!/bin/bash
+    ./LocalAdmin \${SERVER_PORT}" >>start.sh
+  echo "$(tput setaf 4)Finished configuring start.sh for LocalAdmin.$(tput setaf 0)"
 
 fi
 
@@ -91,14 +111,14 @@ if [ "${INSTALL_BOT}" == "true" ]; then
 
   if [ "${CONFIG_SAVER}" == "true" ]; then
     echo "$(tput setaf 4)Config Saver is $(tput setaf 2)ENABLED"
-      echo "Making temporary directory"
-      mkdir BotConfigTemp
-      echo "Moving config."
-      mv config.yml ./BotConfigTemp
+    echo "Making temporary directory"
+    mkdir BotConfigTemp
+    echo "Moving config."
+    mv config.yml ./BotConfigTemp
 
   fi
 else
-    echo "$(tput setaf 4)Skipping bot install...$(tput setaf 0)"
+  echo "$(tput setaf 4)Skipping bot install...$(tput setaf 0)"
 fi
 
 if [ "${INSTALL_EXILED}" == "true" ]; then
@@ -123,14 +143,14 @@ if [ "${INSTALL_EXILED}" == "true" ]; then
 
   fi
 else
-    echo "Skipping Exiled installation."
+  echo "Skipping Exiled installation."
 fi
 
 if [ "${REMOVE_UPDATER}" == "true" ]; then
-    echo "Removing Exiled updater."
-    rm .config/EXILED/Plugins/Exiled.Updater.dll
+  echo "Removing Exiled updater."
+  rm .config/EXILED/Plugins/Exiled.Updater.dll
 else
-    echo "Skipping EXILED updater removal."
+  echo "Skipping EXILED updater removal."
 fi
 
 if [ "${INSTALL_INTEGRATION}" == "true" ]; then
@@ -153,41 +173,70 @@ if [ "${INSTALL_INTEGRATION}" == "true" ]; then
   tar xzvf Plugin.tar.gz
   rm Plugin.tar.gz
 
-  else
-    echo "Skipping Discord integration plugin install"
+else
+  echo "Skipping Discord integration plugin install"
 fi
 
-if  [ "${INSTALL_ADMINTOOLS}" == "true" ]; then
-    echo "Removing existing Admin Tools version."
-    rm AdminTools.dll
-    echo "Installing latest Admin Tools"
-    wget https://github.com/Exiled-Team/AdminTools/releases/latest/download/AdminTools.dll
+if [ "${INSTALL_ADMINTOOLS}" == "true" ]; then
+  echo "Removing existing Admin Tools version."
+  rm .config/EXILED/Plugins/AdminTools.dll
+  echo "Installing latest Admin Tools"
+  wget https://github.com/Exiled-Team/AdminTools/releases/latest/download/AdminTools.dll -P .config/EXILED/Plugins
 
 else
-    echo "Skipping Admin Tools install."
+  echo "Skipping Admin Tools install."
 fi
 
 if [ "${INSTALL_UTILITIES}" == "true" ]; then
-    echo "Removing existing Common Utilities version."
-    rm Common_Utilities.dll
-    echo "Installing Common Utilities."
-    wget https://github.com/Exiled-Team/Common-Utils/releases/latest/download/Common_Utilities.dll
+  echo "Removing existing Common Utilities version."
+  rm .config/EXILED/Plugins/Common_Utilities.dll
+  echo "Installing Common Utilities."
+  wget https://github.com/Exiled-Team/Common-Utils/releases/latest/download/Common_Utilities.dll -P .config/EXILED/Plugins
 else
-    echo "Skipping Common Utilities Install"
+  echo "Skipping Common Utilities Install"
 fi
 
 if [ "${INSTALL_SCPSTATS}" == "true" ]; then
-    echo "Removing existing SCPStats version."
-    rm SCPStats.dll
-    echo "Installing SCPStats"
-    wget https://github.com/SCPStats/Plugin/releases/latest/download/SCPStats.dll
+  echo "Removing existing SCPStats version."
+  rm .config/EXILED/Plugins/SCPStats.dll
+  echo "Installing SCPStats"
+  wget https://github.com/SCPStats/Plugin/releases/latest/download/SCPStats.dll -P .config/EXILED/Plugins
 else
-    echo "Skipping SCPStats Install."
-fi    
+  echo "Skipping SCPStats Install."
+fi
 
-if [ "${INSTALL_BOT}" == "true" ]
-then
-    echo "Dont forget to configure the discord bot in /home/container/config.yml"
+function pluginInstall() {
+  # Caches the plugin to a json so only one request to Github is needed
+
+  curl --silent "$1" | jq . > plugin.json
+
+  echo "Installing $(jq -r .assets[0].name plugin.json) $(jq -r .tag_name plugin.json) by $(jq -r .author.login plugin.json)"
+
+  # For the evil people that put the version in their plugin name the old version will need to be manually deleted
+  rm .config/EXILED/Plugins/"$(jq -r .assets[0].name plugin.json)"
+
+  jq -r .assets[0].browser_download_url plugin.json
+
+  wget "$(jq -r .assets[0].browser_download_url plugin.json)" -P .config/EXILED/Plugins
+
+  rm plugin.json
+}
+
+if [ "${INSTALL_CUSTOM}" == "true" ]; then
+  touch .egg/customplugins.txt || echo "customplugins.txt already exists"
+
+  grep -v '^ *#' <.egg/customplugins.txt | while IFS= read -r I; do
+    pluginInstall "${I}"
+  done
+
+fi
+
+if [ "${INSTALL_BOT}" == "true" ]; then
+  echo "Dont forget to configure the discord bot in /home/container/config.yml"
 fi
 
 echo "$(tput setaf 2)Installation Complete!$(tput sgr 0)"
+
+##TODO - using git to sync configs with github
+
+##TODO - Fix plugin install locations
