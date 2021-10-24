0cd4adc0a083d073550e4a1c11595940aa935fb8
Parkeymon <pwolfe@stealthturtle.com>
2021-10-24 11:49:46 -0700

    it works! undo change

--- script.sh
+++ script.sh
@@ -63,6 +63,7 @@ if [ "${BETA_TAG}" == "none" ]; then
   ./steamcmd.sh +login anonymous +force_install_dir /mnt/server +app_update "${SRCDS_APPID}" validate +quit
 else
   ./steamcmd.sh +login anonymous +force_install_dir /mnt/server +app_update "${SRCDS_APPID}" -beta "${BETA_TAG}" validate +quit
+fi
 
 # Install SL with SteamCMD
 cd /mnt/server || {
